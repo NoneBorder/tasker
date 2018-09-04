26005f4a23382eb1e96d5077936820360ecc8ca7
Guy Cheung <guy.xtafcusqt@gmail.com>
2018-09-04 16:08:20 +0800

    1. 对 consumer 增加context 参数传入（不兼容更新）; 2. 增加正常日志、任务计划执行时间等

--- task.go
+++ task.go
@@ -1,6 +1,7 @@
 package tasker
 
 import (
+	"context"
 	"errors"
 	"time"
 
@@ -16,12 +17,18 @@ const (
 	TaskStatSuccess = "success"
 )
 
+const (
+	TaskCtxKeyLog       = "stdout"          // normal log record
+	TaskCtxKeyExecAfter = "task-exec-after" // plan task exec time
+)
+
 // Task is the core object for tasker package.
 type Task struct {
 	Id         uint64
 	Topic      string
 	Status     string
 	Timeout    int // 超时ms
+	ExecAfter  time.Time `orm:"type(datetime);default(1971-01-01 00:00:00)"`
 	Retry      int
 	Input      string    `orm:"type(text)"`
 	WorkerId   uint64
@@ -29,10 +36,11 @@ type Task struct {
 	Created    time.Time `orm:"auto_now_add"`
 	Updated    time.Time `orm:"auto_now"`
 	Log        string    `orm:"type(text)"`
+	Errlog     string    `orm:"type(text)"`
 }
 
 // ConsumeFn represents consume func definition.
-type ConsumeFn func(Input string, WorkerID uint64) (err error)
+type ConsumeFn func(ctx *context.Context, Input string, WorkerID uint64) (err error)
 
 func (self *Task) TableIndex() [][]string {
 	return [][]string{
@@ -47,12 +55,13 @@ func (self *Task) TableEngine() string {
 // NewSimpleTask is new task with default settings.
 func NewSimpleTask(topic, input string) *Task {
 	return &Task{
-		Topic:    topic,
-		Status:   TaskStatPending,
-		Timeout:  5000,
-		Retry:    3,
-		Input:    input,
-		WorkerId: 0,
+		Topic:     topic,
+		Status:    TaskStatPending,
+		Timeout:   5000,
+		ExecAfter: zeroTime,
+		Retry:     3,
+		Input:     input,
+		WorkerId:  0,
 	}
 }
 
@@ -65,16 +74,16 @@ func (self *Task) Publish() (err error) {
 	return
 }
 
-func (self *Task) exec(fn ConsumeFn) (err error) {
+func (self *Task) exec(ctx *context.Context, fn ConsumeFn) (err error) {
 	errC := make(chan error)
 	go func() {
-		errC <- fn(self.Input, self.WorkerId)
+		errC <- fn(ctx, self.Input, self.WorkerId)
 	}()
 
 	select {
 	case err = <-errC:
-	case <-time.After(time.Duration(self.Timeout) * time.Millisecond):
-		err = errors.New("execute task timeout")
+	case <-(*ctx).Done():
+		err = (*ctx).Err()
 	}
 
 	return
@@ -82,17 +91,24 @@ func (self *Task) exec(fn ConsumeFn) (err error) {
 
 func (self *Task) consume(fn ConsumeFn) bool {
 	startT := time.Now().Local()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(self.Timeout+1)*time.Microsecond)
+	defer cancel()
 
-	err := self.exec(fn)
+	err := self.exec(&ctx, fn)
 	success := err == nil
 
 	if !success {
 		// failed
-		self.Log += err.Error() + "\n"
+		self.Errlog += err.Error() + "\n"
 		self.Status = TaskStatFailed
 		self.Retry -= 1
 		if self.Retry > 0 {
 			self.Status = TaskStatRetry
+
+			// when the job will retry, you can update the next exec time
+			if execAfter, ok := ctx.Value(TaskCtxKeyExecAfter).(time.Time); ok {
+				self.ExecAfter = execAfter
+			}
 		}
 	} else {
 		self.Status = TaskStatSuccess
@@ -101,8 +117,15 @@ func (self *Task) consume(fn ConsumeFn) bool {
 	// stat task exec
 	Stat(self.Topic, self.Status, time.Since(startT))
 
-	// update db record
+	// workerid set to 0
 	self.WorkerId = 0
+
+	// set stdout log from ctx
+	if log, ok := ctx.Value(TaskCtxKeyLog).(string); ok && log != "" {
+		self.Log = log
+	}
+
+	// update db record
 	o := orm.NewOrm()
 	for i := 0; i < 3; i++ {
 		if _, err := o.Update(self); err != nil {
@@ -120,9 +143,9 @@ func Consume(topic string, fn ConsumeFn) (int, error) {
 	o := orm.NewOrm()
 	if IsMaster {
 		// 仅在 master 上对已经僵死的 task 进行重置，减轻 DB 压力
-		if _, err := o.Raw(`UPDATE task SET status=?, worker_id=0 WHERE topic=? AND status=?
+		if _, err := o.Raw(`UPDATE task SET status=?, retry=retry-1, worker_id=0 WHERE topic=? AND status=?
 		AND TIMESTAMPDIFF(SECOND, updated, now())*1000-5000>timeout`,
-			TaskStatPending, topic, TaskStatRunning,
+			TaskStatRetry, topic, TaskStatRunning,
 		).Exec(); err != nil {
 			dora.Error().Msgf("update dead running task to waiting failed: %s", err.Error())
 		}
@@ -139,7 +162,7 @@ func Consume(topic string, fn ConsumeFn) (int, error) {
 	}
 
 	res, err := o.Raw(`UPDATE task SET status=?, worker_id=?, worker_fqdn=concat(worker_fqdn, ?) WHERE
-		topic=? AND worker_id=0 AND status IN (?,?) LIMIT ?`,
+		topic=? AND worker_id=0 AND status IN (?,?) AND exec_after > NOW() LIMIT ?`,
 		TaskStatRunning, workerID, FQDN()+"\n", topic, TaskStatPending, TaskStatRetry, maxConsumeTask,
 	).Exec()
 	if err != nil {
@@ -171,4 +194,14 @@ func Consume(topic string, fn ConsumeFn) (int, error) {
 	}
 
 	return len(tasks), nil
+}
+
+// ConsumeCtxStdout write stdout log to context for record
+func ConsumeCtxStdout(ctx *context.Context, log string) {
+	*ctx = context.WithValue(*ctx, TaskCtxKeyLog, log)
+}
+
+// ConsumeCtxExecAfter update run time for next retry
+func ConsumeCtxExecAfter(ctx *context.Context, t time.Time) {
+	*ctx = context.WithValue(*ctx, TaskCtxKeyExecAfter, t)
 }
